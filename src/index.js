6072cf55b47e02c3a8de50fec6d42c99cdb54c72
Pål Hamel <pal.hamel@gmail.com>
2020-10-22 14:00:26 +0200

    setup react chart

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+// import App from './components/colorChart'
+import Chartcolor from './components/colorChart'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Chartcolor />
   </React.StrictMode>,
   document.getElementById('root')
 );
